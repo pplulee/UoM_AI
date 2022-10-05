ca1ed33989774294e212ed55e0ebe61c95dd237e
Francisco Lobo <francisco.lobo@manchester.ac.uk>
2022-10-05 17:02:30 +0100

    added refresh.sh for lab1

---
+++ refresh.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+gitRemote="base"
+gitUrl="https://gitlab.cs.man.ac.uk/a21674fl/comp24011.git"
+gitBranch="lab1"
+
+set -e
+git remote remove "$gitRemote" || true
+git remote add "$gitRemote" "$gitUrl"
+git fetch "$gitRemote"
+git merge "$gitRemote/$gitBranch"
+
+# vim:set et sw=2 ts=2:
