a77592438fa2455900c17208b1299535337bc988
Francisco Lobo <francisco.lobo@manchester.ac.uk>
2022-10-20 14:34:48 +0100

    added lab2 materials

---
+++ constraintsLab.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+
+# Task 1    
+def Travellers(List):
+    return []
+
+
+# Task 2
+def CommonSum(n):
+    return 0
+
+
+# Task 3
+def msqList(m, pairList):
+    return []
+
+
+# Task 4
+def pmsList(m, pairList):
+    return []
+
+
+# Debug
+if __name__ == '__main__':
+    print("debug run...")
+
