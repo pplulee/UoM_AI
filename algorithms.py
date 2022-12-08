9eb57a22465570387be890d25da7521d0bf01468
pplulee <46434255+pplulee@users.noreply.github.com>
2022-12-08 23:25:50 +0000

    implements function1 and 2

--- algorithms.py
+++ algorithms.py
@@ -5,12 +5,32 @@ import cv2
 
 def dist_thresholding(des1, des2, threshold_value) -> list:
     bf = cv2.BFMatcher()
-    return []
+    matches = bf.knnMatch(des1, des2,k=len(des1))
+    result = []
+    for m in matches:
+        tmp=[]
+        for n in m:
+            if threshold_value==-1:
+                tmp.append(n)
+            elif n.distance < threshold_value:
+                tmp.append(n)
+        result.append(tmp)
+    return result
 
 
 def nn(des1, des2, threshold_value) -> list:
     bf = cv2.BFMatcher()
-    return []
+    matches = bf.knnMatch(des1, des2, k=1)
+    result = []
+    for m in matches:
+        tmp = []
+        for n in m:
+            if threshold_value == -1:
+                tmp.append(n)
+            elif n.distance < threshold_value:
+                tmp.append(n)
+        result.append(tmp)
+    return result
 
 
 def nndr(des1, des2, threshold_value) -> list:
