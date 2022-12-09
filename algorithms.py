f434066552b27dfe75f414975c0b0d2ad5ab8445
pplulee <46434255+pplulee@users.noreply.github.com>
2022-12-09 22:13:17 +0000

    implements function3

--- algorithms.py
+++ algorithms.py
@@ -1,16 +1,17 @@
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 import cv2
 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 def dist_thresholding(des1, des2, threshold_value) -> list:
     bf = cv2.BFMatcher()
-    matches = bf.knnMatch(des1, des2,k=len(des1))
+    matches = bf.knnMatch(des1, des2, k=len(des1))
     result = []
     for m in matches:
-        tmp=[]
+        tmp = []
         for n in m:
-            if threshold_value==-1:
+            if threshold_value == -1:
                 tmp.append(n)
             elif n.distance < threshold_value:
                 tmp.append(n)
@@ -35,7 +36,14 @@ def nn(des1, des2, threshold_value) -> list:
 
 def nndr(des1, des2, threshold_value) -> list:
     bf = cv2.BFMatcher()
-    return []
+    matches = bf.knnMatch(des1, des2, k=2)
+    result = []
+    for i in range(len(matches)):
+        tmp = []
+        if matches[i][0].distance < threshold_value * matches[i][1].distance:
+            tmp.append(matches[i][0])
+        result.append(tmp)
+    return result
 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # vim:set et sw=4 ts=4:
